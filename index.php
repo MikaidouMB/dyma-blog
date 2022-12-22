91729ca29fcc15b9ad068beaa24e90147ba2db68
Mikaïdou <mikaidoumbo@hotmail.fr>
2022-12-22 12:03:34 +0100

    Displaying the list of articles

--- index.php
+++ index.php
@@ -1,18 +1,39 @@
+<?php
+$filename = __DIR__ . '/data/articles.json';
+$articles = [];
+$categories = [];
+
+if (file_exists($filename)) {
+  $articles = json_decode(file_get_contents($filename), true) ?? [];
+  $categories = array_map(fn($a) => $a['category'], $articles);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
+  <head>
       <?php require_once 'includes/head.php' ?>
       <link rel="stylesheet" href="/public/css/index.css">
       <title>Blog</title>
-    </head>
+  </head>
 
-    <body>
-      <div class="container">
-        <?php require_once 'includes/header.php' ?>
-        <div class="content">
+  <body>
+    <div class="container">
+      <?php require_once 'includes/header.php' ?>
+      <div class="content">
+        <div class="articles-container">
+          <?php foreach ($articles as $a) : ?>
+            <div class="article block">
+              <div class="overflow">
+                <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+              </div>
+              <h2><?= $a['title'] ?></h2>
+            </div>
+          <?php endforeach; ?>
         </div>
-        <?php require_once 'includes/footer.php' ?>
       </div>
+      <?php require_once 'includes/footer.php' ?>
+    </div>
 
-    </body>
+  </body>
 </html>
